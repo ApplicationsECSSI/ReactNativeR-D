c3a8ce473f1143190b1679e8e0839365758e2f1f
Shiva Kumar Kallapalli <ShivaK@ecssi.local>
2024-11-13 14:53:12 +0530

    First Push using expo

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+// App.js
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen.js'; // Import the login screen
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
